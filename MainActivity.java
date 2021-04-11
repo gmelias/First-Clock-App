fb240a955a7fe7bff14c46dbd73c8ae0379bb86a
gmelias <73002982+gmelias@users.noreply.github.com>
2021-04-10 18:25:47 -0700

    First Attempt with Android Studio

---
+++ MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.firstclockapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextClock;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickGermany(View view) {
+        TextClock clock = findViewById(R.id.my_clock);
+        clock.setTimeZone("Europe/Berlin");
+    }
+    public void onClickIndia(View view) {
+        TextClock clock = findViewById(R.id.my_clock);
+        clock.setTimeZone("Asia/Kolkata");
+    }
+
+    public void onClickPersonal(View view) {
+        TextClock clock = findViewById(R.id.my_clock);
+        clock.setTimeZone("Canada/Pacific");
+    }
+}
